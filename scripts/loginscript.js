742c1d8bfc4c56697791664b5a25df06c9004a72
Julikogan <163456780+Julikogan@users.noreply.github.com>
2024-10-14 10:05:49 -0300

    script login

---
+++ scripts/loginscript.js
@@ -0,0 +1,102 @@
+function IrAnalisis() {
+    document.location.href="PopUpSubirFoto.html";
+}
+function validarFormularioLI() {
+ 
+    var mail = document.getElementById("mail").value;
+    var contra = document.getElementById("contra").value;
+
+
+    if (mail === "") {
+        alert("Por favor, ingresa tu correo electrónico.");
+        return false;
+    }
+
+
+    // Verificar que el email sea válido
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!emailPattern.test(mail)) {
+        alert("Por favor, ingresa un correo electrónico válido.");
+        return false;
+    }
+
+
+   
+    if (contra === "" ){
+        alert("Porfavor, ingrese su contraseña")
+        return false;
+    }
+    return true;
+}
+function enviarDatosLogIn() {
+    console.log("entro a la funcion enviar datos Log In")
+   const mail = document.getElementById("mail");
+   const contra = document.getElementById("contra");
+
+
+    // const telefono = document.querySelector("input[name='telefono']").value;
+    // const email = document.querySelector("input[name='email']").value;
+    // const contrasena = document.querySelector("input[name='contrasena']").value;
+
+
+    const data = {
+        mail: mail.value,
+        contra: contra.value
+       
+    };
+console.log(data);
+    fetch("router.post("/login", authController.login);", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    })
+    .then((response) => {
+        if (response.status === 200) {
+            console.log("200")
+            return response.json();
+        } else if (response.status === 400) {
+            console.log("Error: No se pudo completar la solicitud.");
+            alert("Alguno de los datos introducidos es incorrecto")
+        }
+    })
+    .then((data) => {
+        if (data && data.token){
+            console.log("Datos enviados exitosamente:", data);
+            localStorage.setItem("token", data.token);
+            alert("LogIn exitoso.");
+            IrAnalisis();
+
+
+        }
+       
+    })
+    .catch((error) => {
+        console.log("hubo error");
+        console.error(error);
+        alert("Hubo un problema con el envío de los datos.");
+    });
+}
+function ConfirmarLogin (){
+    document.getElementById("form").addEventListener("submit", function(event) {
+        event.preventDefault(); // Evitar que la página se recargue
+        console.log("entra");
+   
+        // Llamamos a la función de validación de datos
+        if (validarFormularioLI()) {
+            // Si los datos son válidos, enviamos los datos a la API
+            enviarDatosLogIn();
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
